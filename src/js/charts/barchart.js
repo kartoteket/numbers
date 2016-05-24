38cdec9b543daa1b0c443c95dca52b0fcbf0ab01
Svale Fossåskaret <svale@3by5.no>
2016-05-25 00:10:47 +0200

    Add barchart as reusable barchart module
    A testcase...

---
+++ src/js/charts/barchart.js
@@ -0,0 +1,75 @@
+module.exports = barChart;
+
+
+function barChart () {
+
+  // modules
+  var d3 = require('d3');
+
+  //config options
+  var width       = 500
+    , height      = 300
+    , padding     = 2
+    , fillColor   = 'coral'
+    , data        = []
+  ;
+
+  // generate chart
+  function chart(selection) {     // ref: https://bost.ocks.org/mike/chart/#implementation
+
+        selection.each(function () {
+
+          var barSpacing = height / data.length;
+          var barHeight = barSpacing - padding;
+          var maxValue = d3.max(data);
+          var widthScale = width / maxValue;
+
+          var dom = d3.select(this);
+          var svg = dom.append('svg')
+              .attr('class', 'chart barchart')
+              .attr('height', height)
+              .attr('width', width)
+              .style('fill', fillColor);
+
+          var bars = svg.selectAll('rect.chart__bar')
+              .data(data)
+              .enter()
+              .append('rect')
+              .attr('class', 'chart__bar')
+              .attr('y', function (d, i) { return i * barSpacing;  })
+              .attr('height', barHeight)
+              .attr('x', 0)
+              .attr('width', function (d) { return d * widthScale; });
+
+        });
+
+  }
+
+  // getter//setters
+  chart.width = function(value) {
+    if (!arguments.length) return width;
+    width = value;
+    return chart;
+  };
+
+  chart.height = function(value) {
+    if (!arguments.length) return height;
+    height = value;
+    return chart;
+  };
+
+  chart.fillColor = function(value) {
+    if (!arguments.length) return fillColor;
+    fillColor = value;
+    return chart;
+  };
+
+  chart.data = function(value) {
+    if (!arguments.length) return data;
+    data = value;
+    return chart;
+  };
+
+  return chart;
+
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
-var x = test;
-console.log(x);
+// modules
+var d3  = require('d3');
+var barchart  = require('./charts/barchart.js');
+
+// bootstrap charts
+var chartOne =  barchart().data([10, 20, 30, 40,10, 20, 60]);
+var chartTwo =  barchart()
+                    .width(500)
+                    .height(500)
+                    .fillColor('cyan')
+                    .data([20, 50, 10, 20,10, 50, 60]);
+
+
+d3.select('.chart-one')
+    .call(chartOne);
+
+d3.select('.chart-two')
+    .call(chartTwo);
