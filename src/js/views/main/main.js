ecb6bca52d72419dd861796585ba9647cd35cafc
Svale Fossåskaret <svale@3by5.no>
2016-08-16 10:15:13 +0200

    Loading map data

--- src/js/views/main/main.js
+++ src/js/views/main/main.js
@@ -1,5 +1,6 @@
 var Ractive   = require('ractive')
   , lMap      = require('components/map/map')
+  , svgMap    = require('components/map/svgmap')
   , pieChart  = require('components/charts/PieChart')
   , barGraph  = require('components/charts/HorisontalBarGraph')
   , lineChart  = require('components/charts/lineChart')
@@ -11,14 +12,15 @@ main = Ractive.extend({
   template: require('./template.html'),//temp,//'<div>{{message}}</div>',
   data: {
     initialData: 'world_innvandring.json',
-    initialBarData: 'asylsoknader_uke_21.json'
+    initialBarData: 'asylsoknader_uke_21.json',
+    initialMapData: 'asyl_eur_2016.json',
     initialLineData: 'line-chart-test-data.json'
   },
 
   components: {
-    Map: lMap,
-    PieChart: pieChart,
-    BarGraph: barGraph
+   Map: svgMap,
+//   PieChart: pieChart,
+   BarGraph: barGraph,
    LineChart: lineChart
   },
 
